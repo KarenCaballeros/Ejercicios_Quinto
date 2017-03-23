75d5f8e662f82930a4f1066e65386ea7812a7134
KarenCaballeros <anakarencaballeros@gmail.com>
2017-03-23 11:44:39 -0600

    agregue

--- django/academico/academico/grados/views.py
+++ django/academico/academico/grados/views.py
@@ -6,3 +6,8 @@ def lista_grados(request):
 	grados = Grado.objects.all()
 	return render(request, 'lista_grados.html',
 		{'grados' : grados})
+
+def detalle_grado(request, grado_pk):
+	grado = Grado.objects.grado_pk()
+	return render(request, 'detalle_grado.html',
+		{'estudiantes' : estudiantes})
